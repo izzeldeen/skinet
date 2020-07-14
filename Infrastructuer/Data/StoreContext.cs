c2a7b81ef434200f957e0fe60e47a8c56521f016
izzeldeen kalbouneh <izzeldeen_kalbouneh@outlook.com>
2020-07-14 11:15:21 +0300

    Intial Commit

---
+++ Infrastructuer/Data/StoreContext.cs
@@ -0,0 +1,17 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructuer.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        
+        
+    }
+}
